08aedf9f0b8bca5979f6dbd7565de659e3133c5f
kushk4856 <kushk4998@gmail.com>
2025-10-16 22:35:42 +0530

    signup form setup

--- src/_auth/forms/SingupForm.tsx
+++ src/_auth/forms/SingupForm.tsx
@@ -1,8 +1,8 @@
-import * as z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { Button } from "../../components/ui/button"
-import { useForm } from "react-hook-form"
+import { Button } from "../../components/ui/button";
+import { useForm } from "react-hook-form";
 
 import {
   Form,
@@ -12,57 +12,122 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+} from "@/components/ui/form";
+import { Input } from "../../components/ui/input";
+import { SignupValidation } from "../../lib/validation";
+import Loader from "../../components/shared/Loader";
+import { Link } from "react-router-dom";
 
 const SingupForm = () => {
- // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const isLoading = false;
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
-  })
- 
+  });
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof SingupForm>) {
+    // const newUser = await createUserAccount(values)
   }
 
   return (
-   <div>
-     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-   </div>
-  )
-}
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="" />
 
-export default SingupForm
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-4">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use SocialApp enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} className="shad-input" />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} className="shad-input" />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} className="shad-input" />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} className="shad-input" />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {
+              isLoading ? (
+                <div className="flex-center gap-2">
+                  <Loader/> Loading..
+                </div>
+              ): "Sign up"
+            }
+          </Button>
+
+            <p className="text-small-regular text-light-2 text-center mt-2">
+              Already have an account?
+              <Link to="/sign-in" className="text-primary-500 text-small-smibold ml-1">Log in</Link>
+            </p>
+
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SingupForm;
